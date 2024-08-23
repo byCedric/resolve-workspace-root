07d4f11c8df0f431d29d0c8fa9754e68c650d428
Cedric van Putten <github@cedric.dev>
2024-08-23 09:37:43 +0200

    fix: ignore `EISDIR` errors when reading files

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -4,7 +4,7 @@ export function tryReadFile(file: string): string | null {
   try {
     return fs.readFileSync(file, { encoding: 'utf-8' });
   } catch (error: any) {
-    if (error.code === 'ENOENT') {
+    if (shouldIgnoreError(error)) {
       return null;
     }
 
@@ -14,10 +14,18 @@ export function tryReadFile(file: string): string | null {
 
 export function tryReadFileAsync(file: string): Promise<string | null> {
   return fs.promises.readFile(file, { encoding: 'utf-8' }).catch((error: any) => {
-    if ('code' in error && error.code === 'ENOENT') {
+    if (shouldIgnoreError(error)) {
       return null;
     }
 
     throw error;
   });
+}
+
+/**
+ * We are optimistically reading files, avoiding unnecessary file lookup checks.
+ * Some thrown errors are expected and should be ignored as "file not found".
+ */
+function shouldIgnoreError(error: any): error is Error {
+  return 'code' in error && (error.code === 'ENOENT' || error.code === 'EISDIR');
 }
